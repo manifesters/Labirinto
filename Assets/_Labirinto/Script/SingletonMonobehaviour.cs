a57294b742483c70b21323d730cb865e406fb8e1
Jonathan <jb.jonathanbarsaga@gmail.com>
2024-07-04 16:37:05 +0800

    Added SingletonMonoBehavior Script

---
+++ Assets/_Labirinto/Script/SingletonMonobehaviour.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SingletonMonobehaviour <T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance => _instance;
+    public bool dontDestroyOnLoad = false;
+
+    public virtual void Awake() 
+    {
+        _instance = this as T;
+        if(dontDestroyOnLoad)
+        {
+            DontDestroyOnLoad(gameObject);
+        }    
+    }
+}
